a6d61a05503a10100500938f0a90f260b0b3ac00
Viktor <svb28rus@yandex.ru>
2025-06-08 13:40:38 +0300

    11

---
+++ JavaMarathon2020/src/main/java/day0/Task1.java
@@ -0,0 +1,11 @@
+package day0;
+
+public class Task1 {
+    public static void main(String[] args){
+        int count = 0;
+        while (count < 10){
+            System.out.print("JAVA" + " ");
+            count++;
+        }
+    }
+}
---
+++ JavaMarathon2020/src/main/java/day1/Task3.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        for(int a = 0; a<10; a++){
+            System.out.print("JAVA \n");
+        }
+
+    }
+}
